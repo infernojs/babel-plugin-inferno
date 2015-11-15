84fd96e0dfd5c8bc1f9c5942f2f6a67b646f1c5b
Dominic Gannaway <dominic@101010digital.com>
2015-11-15 22:22:56 +0000

    fixed issue with StringLiterals

--- example.js
+++ example.js
@@ -5,6 +5,26 @@ var plugin = require("./lib/index");
 
 
 
+//
+//var code = `
+//    var foo = require("test");
+//
+//    var someObj = {
+//        render() {
+//            return (
+//                <div>Hey world!</div>
+//            );
+//        }
+//    }
+//`;
+//
+//var output = babel.transform(code, {
+//    plugins: [plugin, 'syntax-jsx'],
+//    presets: ['es2015']
+//}).code;
+//
+//console.log(output);
+
 
 var code = `
     var foo = require("test");
@@ -12,7 +32,7 @@ var code = `
     var someObj = {
         render() {
             return (
-                <div>Hey world!</div>
+                <div autoFocus='true' />
             );
         }
     }
@@ -25,84 +45,87 @@ var output = babel.transform(code, {
 
 console.log(output);
 
-var code = `
-    var foo = require("test");
-
-    var someObj = {
-        render() {
-            return (
-                <ul><li>Im a li-tag</li><li>Im a li-tag</li><li>Im a li-tag</li></ul>
-            );
-        }
-    }
-`;
-
-var output = babel.transform(code, {
-    plugins: [plugin, 'syntax-jsx'],
-    presets: ['es2015']
-}).code;
-
-console.log(output);
 
 
-var code = `
-    var foo = require("test");
-
-    var someObj = {
-        render() {
-            return (
-                <div><div><span>{ test || foo }</span></div><h1>Hello world!</h1></div>
-            );
-        }
-    }
-`;
-
-var output = babel.transform(code, {
-    plugins: [plugin, 'syntax-jsx'],
-    presets: ['es2015']
-}).code;
-
-console.log(output);
-
-var code = `
-    Inferno.render(<div><span>{ test || foo }</span>{ bar } { foo }</div>, foo);
-`;
-
-var output = babel.transform(code, {
-    plugins: [plugin, 'syntax-jsx'],
-    presets: ['es2015']
-}).code;
-
-console.log(output);
-
-var code = `
-    var foo = require("test");
-
-    var someObj = {
-        render() {
-            return (
-                <div test1={ foo1 }
-                    test2={ "test" }
-                    test3={ foo.bind(this) }
-                    test4={ () => "lol" }
-                    test5="123"
-                    test6={ too\`bar\` }
-                ><Foo
-                    test1={ a ? 1 : 2 }
-                    test2={ [1,2,3] }
-                    test3={ {"bar": foo2} }
-                    test4={ \`test\` }
-                    test5={ 1 + 1 }
-                    test6={ \`foo$\{ fooBar \}\` }></Foo
-                ></div>
-            );
-        }
-    }
-`;
-
-var output = babel.transform(code, {
-    plugins: [plugin, 'syntax-jsx'],
-    presets: ['es2015']
-}).code;
-
-console.log(output);
+//
+//var code = `
+//    var foo = require("test");
+//
+//    var someObj = {
+//        render() {
+//            return (
+//                <ul><li>Im a li-tag</li><li>Im a li-tag</li><li>Im a li-tag</li></ul>
+//            );
+//        }
+//    }
+//`;
+//
+//var output = babel.transform(code, {
+//    plugins: [plugin, 'syntax-jsx'],
+//    presets: ['es2015']
+//}).code;
+//
+//console.log(output);
+//
+//
+//var code = `
+//    var foo = require("test");
+//
+//    var someObj = {
+//        render() {
+//            return (
+//                <div><div><span>{ test || foo }</span></div><h1>Hello world!</h1></div>
+//            );
+//        }
+//    }
+//`;
+//
+//var output = babel.transform(code, {
+//    plugins: [plugin, 'syntax-jsx'],
+//    presets: ['es2015']
+//}).code;
+//
+//console.log(output);
+//
+//var code = `
+//    Inferno.render(<div><span>{ test || foo }</span>{ bar } { foo }</div>, foo);
+//`;
+//
+//var output = babel.transform(code, {
+//    plugins: [plugin, 'syntax-jsx'],
+//    presets: ['es2015']
+//}).code;
+//
+//console.log(output);
+//
+//var code = `
+//    var foo = require("test");
+//
+//    var someObj = {
+//        render() {
+//            return (
+//                <div test1={ foo1 }
+//                    test2={ "test" }
+//                    test3={ foo.bind(this) }
+//                    test4={ () => "lol" }
+//                    test5="123"
+//                    test6={ too\`bar\` }
+//                ><Foo
+//                    test1={ a ? 1 : 2 }
+//                    test2={ [1,2,3] }
+//                    test3={ {"bar": foo2} }
+//                    test4={ \`test\` }
+//                    test5={ 1 + 1 }
+//                    test6={ \`foo$\{ fooBar \}\` }></Foo
+//                ></div>
+//            );
+//        }
+//    }
+//`;
+//
+//var output = babel.transform(code, {
+//    plugins: [plugin, 'syntax-jsx'],
+//    presets: ['es2015']
+//}).code;
+//
+//console.log(output);
