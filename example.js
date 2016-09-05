9fcd0ee062143f2b64a368f6cd476d90e52f8f97
Dominic Gannaway <dg@domgan.com>
2016-09-06 00:52:19 +0100

    WIP

--- example.js
+++ example.js
@@ -4,7 +4,24 @@ const babel = require('babel-core');
 const plugin = require('./lib/index');
 
 const code = `
-	<div><div><div></div></div></div>
+		function render() {
+			return (
+				<div style={ this.props.styles }>
+					<span style={ this.props.styles }>The title is { this.props.title }</span>
+				</div>
+			);
+		}
+		render((
+			<BasicComponent3 title="styled!" styles={ { color: 'red', paddingLeft: '10px' } } />
+		), container);
+
+		render((
+			<BasicComponent3 />
+		), container);
+
+		render((
+			<BasicComponent3 title="styled (again)!" styles={ { color: 'blue', marginBottom: '20px' } } />
+		), container);
 `;
 
 const output = babel.transform(code, {
