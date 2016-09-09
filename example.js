95d44db9f7740de2ff9543844d41a312aa7b3fbd
Dominic Gannaway <dg@domgan.com>
2016-09-09 11:08:03 +0100

    better handles data-something attributes

--- example.js
+++ example.js
@@ -4,10 +4,7 @@ const babel = require('babel-core');
 const plugin = require('./lib/index');
 
 const code = `
-	var a = <div className={ abc }>foo</div>
-	var b = <div className={ abc }>foo</div>
-	var c = <div>{ lol }</div>
-	var c = <div>foo<div>foo</div></div>
+	var a = <div className={ abc } data-ticket-index={ lol }>foo</div>
 `;
 
 const output = babel.transform(code, {
