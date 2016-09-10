539a0834c8ec2c4f5ab96f81aaf15f86d3cae846
Dominic Gannaway <dg@domgan.com>
2016-09-10 20:15:15 +0100

    fixes an issue where static children of components were being skipped

--- example.js
+++ example.js
@@ -4,15 +4,9 @@ const babel = require('babel-core');
 const plugin = require('./lib/index');
 
 const code = `
-	var a = <div>
-		<div>
-		{ foo }
-		</div>
-		{ null }
-		<div>
-		</div>
-	</div>
-	var b = <div><div /></div>
+	var a = <BasicComponent2 title="abc" name="basic-render">
+					<span>Im a child</span>
+				</BasicComponent2>
 `;
 
 const output = babel.transform(code, {
