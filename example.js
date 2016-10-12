9e56c468bcc8c097f235963b648b8e9c6e081602
Dominic Gannaway <dominicgannaway@Dominics-MacBook-Pro.local>
2016-10-12 11:08:16 +0100

    fixes an issue where hashes were not being calculated properly when passed through in a mixed nature. also make the plugin node 4 compliant

--- example.js
+++ example.js
@@ -4,68 +4,16 @@ const babel = require('babel-core');
 const plugin = require('./lib/index');
 
 const code = `
-import Inferno from 'inferno';
-import createClass from 'inferno-create-class';
-import {StaggeredMotion, spring, presets} from '../../src/inferno-motion';
-import range from 'lodash.range';
-
-const Demo = createClass({
-  getInitialState() {
-    return {x: 250, y: 300};
-  },
-
-  componentDidMount() {
-    window.addEventListener('mousemove', this.handleMouseMove);
-    window.addEventListener('touchmove', this.handleTouchMove);
-  },
-
-  handleMouseMove({pageX: x, pageY: y}) {
-    this.setState({x, y});
-  },
-
-  handleTouchMove({touches}) {
-    this.handleMouseMove(touches[0]);
-  },
-
-  getStyles(prevStyles) {
-    const endValue = prevStyles.map((_, i) => {
-      return i === 0
-        ? this.state
-        : {
-            x: spring(prevStyles[i - 1].x, presets.gentle),
-            y: spring(prevStyles[i - 1].y, presets.gentle),
-          };
-    });
-    return endValue;
-  },
-
-  render() {
-    return (
-      <StaggeredMotion
-        defaultStyles={range(6).map(() => ({x: 0, y: 0}))}
-        styles={this.getStyles}>
-        {balls =>
-          <div className="demo1">
-            {balls.map(({x, y}, i) =>
-              <div
-                key={i}
-                style={{
-                  zIndex: balls.length - i,
-                }} />
-            )}
-          </div>
-        }
-      </StaggeredMotion>
-    );
-  },
-});
-
-export default Demo;
-
+export default (
+  <div>
+    <img src="FOO_URL"/><Component>Foo</Component>
+    <img src="BAR_URL"/><Component>Bar</Component>
+  </div>
+);
 `;
 
 const output = babel.transform(code, {
-	plugins: [[ plugin, { inline: true, preclone: true }], 'syntax-jsx' ],
+	plugins: [[ plugin, { inline: false, preclone: true }], 'syntax-jsx' ],
 	presets: ['es2015']
 }).code;
 
