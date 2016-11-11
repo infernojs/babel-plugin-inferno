d3f6900e817693d6774331c0cef1f89097eacb8a
Dominic Gannaway <ADM@Dominics-MacBook-Pro.local>
2016-11-11 21:05:44 +0000

    change made to how isAstNull works

--- example.js
+++ example.js
@@ -4,7 +4,11 @@ const babel = require('babel-core');
 const plugin = require('./lib/index');
 
 const code = `
-	render(<StatelessComponent onComponentWillMount={spyObj.fn}/>, _container);
+render(<input type="text" ref={obj.fn} spellcheck="false"
+									readOnly={bool ? 'readonly' : false} disabled={bool}
+									ondragenter={test} ondragover={test} value={newValue} oninput={test}
+									onclick={obj.click} class="edit-field" onkeydown={test} onkeyup={test}
+									onBlur={test} {...spread} />, container);
 `;
 
 const output = babel.transform(code, {
