0e291e1ad36696e477fe59960c258b8947382582
Dominic Gannaway <dg@domgan.com>
2016-11-12 15:23:11 +0000

    fixes an issue with children as a prop

--- example.js
+++ example.js
@@ -4,11 +4,7 @@ const babel = require('babel-core');
 const plugin = require('./lib/index');
 
 const code = `
-render(<input type="text" ref={obj.fn} spellcheck="false"
-									readOnly={bool ? 'readonly' : false} disabled={bool}
-									ondragenter={test} ondragover={test} value={newValue} oninput={test}
-									onclick={obj.click} class="edit-field" onkeydown={test} onkeyup={test}
-									onBlur={test} {...spread} />, container);
+<StatelessComponent children="Hello world" />
 `;
 
 const output = babel.transform(code, {
