ad451358d77228aa2ac6574cc0667da5732cc2f4
Dominic Gannaway <dg@domgan.com>
2016-12-13 21:24:49 +0000

    fixes string new line issues and also adds a hoisted Inferno.createVNode

--- example.js
+++ example.js
@@ -4,17 +4,32 @@ const babel = require('babel-core');
 const plugin = require('./lib');
 
 const code = `
-				const children = [
-					<B key="b"></B>,
-					<div key="a">ROW</div>
-				];
+      const foo = <div>
+        <p>
+          This should have
+          no new lines in it
+          or spaces at start
+          and end
+        </p>
+        <div>{ ['1 space after this '] }</div>
+      </div>
+
+      const foo2 = <div>
+        <p>
+          This should have
+          no new lines in it
+          or spaces at start
+          and end
+        </p>
+        <div>{ ['1 space after this '] }</div>
+      </div>
 `;
 
 console.log(
   babel.transform(code, {
     presets: ['es2015'],
     plugins: [
-      [plugin, {inline: false, preclone: true}],
+      [plugin],
       'syntax-jsx'
     ]
   }).code
