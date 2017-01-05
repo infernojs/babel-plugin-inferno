25c3193585d18a76329a97359077476ac7b8bf8e
Sampo Kivistö <sampo.kivisto@live.fi>
2017-01-05 19:38:07 +0200

    use import instead global Inferno reference

--- example.js
+++ example.js
@@ -1,15 +1,19 @@
 'use strict';
 
 const babel = require('babel-core');
+
 const plugin = require('./lib');
 
 const code = `
-<div>Hello world, { ['Foo! ', 'Bar!'] }</div>
+<div>
+    <span>Hello</span>
+    <div>Another</div>
+</div>
 `;
 
 console.log(
   babel.transform(code, {
-    presets: ['es2015'],
+    presets: [['es2015', {modules: false}]],
     plugins: [
       [plugin],
       'syntax-jsx'
