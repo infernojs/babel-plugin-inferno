c809fc52e102845b65834e32a000ce3dd88f38f5
Sampo Kivistö <sampo.kivisto@live.fi>
2019-02-20 18:28:46 +0200

    Updated dependencies and reduced vNodeTypes filesize

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,6 @@ import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import { uglify } from "rollup-plugin-uglify";
 import babel from 'rollup-plugin-babel';
-import pkg from './package.json';
 
 export default [
   // browser-friendly UMD build
